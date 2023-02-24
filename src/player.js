64a6d8e11a54e2c26ee1b0ee64a0411c9b9c88fc
khs-kks <95934427+khs-kks@users.noreply.github.com>
2023-02-24 10:59:14 +0100

    Add attacking capabilities of Player

--- src/player.js
+++ src/player.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-continue */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
+import Gameboard from "./gameboard";
+
 export default class Player {
+  static turns = [];
+
   constructor(name = "Computer") {
     this.name = this._checkPlayerName(name);
+    this.gameboard = new Gameboard();
   }
 
   _checkPlayerName(name) {
@@ -11,5 +17,28 @@ export default class Player {
     }
 
     return name.replace(/\s/g, "");
+  }
+
+  manualAttack(opponent, row, column) {
+    if (opponent.gameboard.receiveAttack(row, column)) {
+      Player.turns.push(this.name);
+    }
+  }
+
+  automaticAttack(opponent) {
+    let attackSuccessful = false;
+
+    do {
+      const row = Math.floor(Math.random() * 10);
+      const column = Math.floor(Math.random() * 10);
+
+      try {
+        attackSuccessful = opponent.gameboard.receiveAttack(row, column);
+      } catch {
+        continue;
+      }
+    } while (!attackSuccessful);
+
+    Player.turns.push(this.name);
   }
 }
--- src/player.test.js
+++ src/player.test.js
@@ -5,19 +5,67 @@ import Player from "./player";
 // });
 
 test("default name value", () => {
-    const comp = new Player();
-    expect(comp.name).toMatch("Computer");
-})
+  const comp = new Player();
+  expect(comp.name).toMatch("Computer");
+});
 
 test("if name is empty", () => {
-    expect(() => new Player("")).toThrow('Name cannot be empty');
+  expect(() => new Player("")).toThrow("Name cannot be empty");
 });
 
 test("if name has any whitespace", () => {
-    const player = new Player(" Uaua Uaua Ok  ");
-    const player2 = new Player("AllGood");
-    
-    expect(player.name).not.toMatch(/\s/);
-    expect(player2.name).not.toMatch(/\s/);
-})
+  const player = new Player(" Uaua Uaua Ok  ");
+  const player2 = new Player("AllGood");
 
+  expect(player.name).not.toMatch(/\s/);
+  expect(player2.name).not.toMatch(/\s/);
+});
+
+test("Manual attack method", () => {
+  const player = new Player("Kris");
+  const comp = new Player();
+
+  comp.gameboard.placeShip(3, 3, 5, "vertical");
+  player.gameboard.placeShip(5, 0, 0, "vertical");
+
+  expect(Player.turns.length).toBe(0);
+
+  player.manualAttack(comp, 4, 5);
+
+  expect(Player.turns.length).toBeGreaterThan(0);
+
+  player.manualAttack(comp, 2, 5);
+
+  expect(Player.turns.length).toBe(1);
+
+  expect(Player.turns).toContain("Kris");
+
+  comp.manualAttack(player, 4, 0);
+
+  expect(Player.turns).toContain("Computer");
+});
+
+test("Automatic attack method", () => {
+  Player.turns.length = 0;
+  const player = new Player("Kris");
+  const comp = new Player();
+
+  comp.gameboard.placeShip(3, 3, 5, "vertical");
+  player.gameboard.placeShip(5, 0, 0, "vertical");
+
+  expect(Player.turns.length).toBe(0);
+
+  player.automaticAttack(comp);
+
+  expect(Player.turns.length).toBeGreaterThan(0);
+
+  player.automaticAttack(comp);
+
+  expect(Player.turns.length).toBe(2);
+
+  expect(Player.turns).toContain("Kris");
+
+  comp.automaticAttack(player);
+
+  expect(Player.turns).toContain("Computer");
+});
